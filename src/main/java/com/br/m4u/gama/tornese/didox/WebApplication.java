66ab1a464db447f083c7dc0cf8013a501fd2b8fb
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-09-23 19:29:37 -0300

    iniciando meu projeto java

---
+++ src/main/java/com/br/m4u/gama/tornese/didox/WebApplication.java
@@ -0,0 +1,13 @@
+package com.br.m4u.gama.tornese.didox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/m4u/gama/tornese/didox/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.m4u.gama.tornese.didox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
